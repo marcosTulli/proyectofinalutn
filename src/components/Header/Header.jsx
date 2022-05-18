5f85b94864ac1290ada3c4fb36fa8d593dae4048
marcosTulli <marcostulli@hotmail.com>
2022-05-18 20:41:08 -0300

    Add login function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,11 @@ import { NavLink } from "react-router-dom";
 import CartStore from "../../utils/CartStore";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import CategoriesMenu from "../Categories/Categories";
+import { UnarchiveSharp } from "@material-ui/icons";
 
 const Header = () => {
   const [itemCount, setItemCount] = useState(0);
+  const [logIn, setLogIn] = useState(false);
 
   useEffect(() => {
     CartStore.subscribe(() => {
@@ -21,6 +23,10 @@ const Header = () => {
     });
   }, []);
 
+  const handleLogin = () => {
+    setLogIn(true);
+  };
+
   return (
     <nav
       className="navbar navbar-expand navbar-dark fixed-top bg-dark"
@@ -33,23 +39,34 @@ const Header = () => {
                 <h1>Get Your Tickets</h1>
               </NavLink>
             </li>
+            {!logIn && (
+              <li className="nav-item">
+                <button onClick={handleLogin} className="btn btn-primary">
+                  <h2>login</h2>
+                </button>
+              </li>
+            )}
           </ul>
         </div>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink exact to="/cart" className="nav-link">
-                <AiOutlineShoppingCart className="bi bi-cart-plus-fill text-white font-xxlarge" />
-                <span className="font-upper font-bold text-white ms-4">
-                  <span className="font-xxlarge align-middle">{itemCount}</span>
-                  <span className="align-middle ms-2">Tickets</span>
-                </span>
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        {logIn && (
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink exact to="/cart" className="nav-link">
+                  <AiOutlineShoppingCart className="bi bi-cart-plus-fill text-white font-xxlarge" />
+                  <span className="font-upper font-bold text-white ms-4">
+                    <span className="font-xxlarge align-middle">
+                      {itemCount}
+                    </span>
+                    <span className="align-middle ms-2">Tickets</span>
+                  </span>
+                </NavLink>
+              </li>
+            </ul>
+            <CategoriesMenu />
+          </div>
+        )}
       </div>
-      <CategoriesMenu />
     </nav>
   );
 };
