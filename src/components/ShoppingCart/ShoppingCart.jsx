cc5d452e8d6958441cf8dfec5a1fa7ae2e55b840
marcosTulli <marcostulli@hotmail.com>
2022-05-19 22:30:41 -0300

    Refactored checkout form

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import CartStore from "../../utils/CartStore";
 import { ShoppingCartItem } from "../ShoppingCartItem";
 import { CheckoutForm } from "./../CheckoutForm";
+import "./Shopping.css";
 
 function Shoppingcart() {
   const navigate = useNavigate();
@@ -41,28 +42,152 @@ function Shoppingcart() {
   }, []);
 
   return !cartIsEmpty() ? (
-    <div className="container" id="carttable">
-      <table className="table mt-5">
-        <thead>
-          <tr>
-            <th scope="col">Event</th>
-            <th scope="col">Price</th>
-            <th scope="col"># Tickets</th>
-            <th scope="col">Total</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map((item) => (
-            <ShoppingCartItem event={item} key={item.id} />
-          ))}
-        </tbody>
-        <div> Total: ${totalAmount}</div>
-        <tfoot>
-          <CheckoutForm />
-        </tfoot>
-      </table>
-    </div>
+    <body>
+      <div className="container mb-4">
+        <div className="row pt-2 pb-4">
+          <div className="col-lg-8">
+            <form action="#" />
+            <div className="row pt-4">
+              <div className="col-lg-6 pl-0">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter name"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter email"
+                    required
+                  />
+                </div>
+
+                <div className="form-group">
+                  <input
+                    className="form-control"
+                    rows="5"
+                    placeholder="Enter Address"
+                    id="Address"
+                    required></input>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter city"
+                    required
+                  />
+                </div>
+                <div className="row">
+                  <div className="col-lg-6 pl-0">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter State"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6 pr-0 mob-pl-0">
+                    <div className="form-group">
+                      <input
+                        type="number"
+                        className="form-control"
+                        placeholder="Enter Zip"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 mob-pl-0">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Name of card"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="Enter Card number"
+                    required
+                  />
+                </div>
+                <div className="row">
+                  <div className="col-lg-6 pl-0">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="01/95"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6 pr-0 mob-pl-0">
+                    <div className="form-group">
+                      <input
+                        type="number"
+                        className="form-control"
+                        placeholder="Enter Cvv"
+                        irequired
+                      />
+                    </div>
+                  </div>
+                  <div className="col-md-12 p-0 text-center">
+                    <p className="mb-1 pb-2">Accepted Cards</p>
+                    <img src="payment.png" alt="nothing" className="w-100" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 pt-1">
+            <div className="row pt-4">
+              <div className="col-8">
+                <h3 className="mb-0 pb-2">Cart</h3>
+              </div>
+              <div className="col-4">
+                <h3 className="mb-0 pb-2">4</h3>
+              </div>
+            </div>
+            {cart.map((item) => (
+              <ShoppingCartItem event={item} key={item.id} />
+            ))}
+            <div
+              className="row border-top  border-dark"
+              style={{ paddingTop: "1em" }}>
+              <div className="col-8">
+                <h5 className="pt-1">Total</h5>
+              </div>
+              <div className="col-4">
+                <h5 className="pt-2">${totalAmount}</h5>
+              </div>
+            </div>
+            <div className="row pb-5 pt-4">
+              <div className="col-12">
+                <div className="form-group form-check"></div>
+                <button
+                  type="submit"
+                  className="btn btn-primary w-100"
+                  style={{ fontSize: "2rem", padding: "0" }}>
+                  Order
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
   ) : (
     <div>
       <h1 style={{ paddingTop: "5rem" }}> The cart is empty</h1>
@@ -79,5 +204,44 @@ function Shoppingcart() {
       </tr>
     </div>
   );
+  // return !cartIsEmpty() ? (
+  //   <div className="container" id="carttable">
+  //     <table className="table mt-5">
+  //       <thead>
+  //         <tr>
+  //           <th scope="col">Event</th>
+  //           <th scope="col">Price</th>
+  //           <th scope="col"># Tickets</th>
+  //           <th scope="col">Total</th>
+  //           <th scope="col"></th>
+  //         </tr>
+  //       </thead>
+  //       <tbody>
+  //         {cart.map((item) => (
+  //           <ShoppingCartItem event={item} key={item.id} />
+  //         ))}
+  //       </tbody>
+  //       <div> Total: ${totalAmount}</div>
+  //       <tfoot>
+  //         <CheckoutForm />
+  //       </tfoot>
+  //     </table>
+  //   </div>
+  // ) : (
+  //   <div>
+  //     <h1 style={{ paddingTop: "5rem" }}> The cart is empty</h1>
+  //     <tr className="align-middle">
+  //       <td colSpan="5" className="text-center">
+  //         <button
+  //           type="button"
+  //           id="btnOrder"
+  //           onClick={handleBackClick}
+  //           className="btn btn-primary btn-primary-themed btn-md font-upper">
+  //           Back to events
+  //         </button>
+  //       </td>
+  //     </tr>
+  //   </div>
+  // );
 }
 export default Shoppingcart;
--- src/components/ShoppingCartItem/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem/ShoppingCartItem.jsx
@@ -1,46 +1,87 @@
 import React from "react";
 import CartStore from "../../utils/CartStore";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 const Shoppingcartitem = ({ event }) => {
   return (
-    <tr className="align-middle">
-      <td>
-        {new Date(event.date).toLocaleString()}
-        <br />
-        {event.name}
-        <br />
-        {event.artist}
-      </td>
-      <td className="max-50">${event.price}</td>
-      <td className="max-50">
-        <div className="btn-group">
-          <input
-            type="number"
-            className="w-auto"
-            value={event.quantity}
-            onChange={(e) =>
-              CartStore.dispatch({
-                type: "update",
-                payload: { ...event, quantity: parseInt(e.target.value) },
-              })
-            }
-          />
-        </div>
-      </td>
-      <td>${event.quantity * event.price}</td>
-      <td>
-        <button
-          className="btn btn-link"
-          onClick={() =>
-            CartStore.dispatch({ type: "delete", payload: event })
-          }>
-          <span className="bi bi-trash-fill font-large text-dark">
-            <CloseOutlinedIcon />
-          </span>
-        </button>
-      </td>
-    </tr>
+    <Box sx={{ flexGrow: 1 }} style={{ color: "white" }}>
+      <Grid
+        container
+        spacing={2}
+        style={{ display: "flex", alignItems: "center" }}>
+        <Grid item xs={3}>
+          {event.artist}
+        </Grid>
+        <Grid item xs={3}>
+          <div className="btn-group">
+            <input
+              type="number"
+              style={{ width: "60px" }}
+              value={event.quantity}
+              onChange={(e) =>
+                CartStore.dispatch({
+                  type: "update",
+                  payload: { ...event, quantity: parseInt(e.target.value) },
+                })
+              }
+            />
+          </div>
+        </Grid>
+        <Grid item xs={3}>
+          {event.quantity * event.price}
+        </Grid>
+        <Grid item xs={3}>
+          <button
+            className="btn btn-link"
+            onClick={() =>
+              CartStore.dispatch({ type: "delete", payload: event })
+            }>
+            <span className="bi bi-trash-fill font-large text-dark">
+              <CloseOutlinedIcon />
+            </span>
+          </button>
+        </Grid>
+      </Grid>
+    </Box>
+    // <tr className="align-middle">
+    //   <td>
+    //     {new Date(event.date).toLocaleString()}
+    //     <br />
+    //     {event.name}
+    //     <br />
+    //     {event.artist}
+    //   </td>
+    //   <td className="max-50">${event.price}</td>
+    //   <td className="max-50">
+    //     <div className="btn-group">
+    //       <input
+    //         type="number"
+    //         className="w-auto"
+    //         value={event.quantity}
+    //         onChange={(e) =>
+    //           CartStore.dispatch({
+    //             type: "update",
+    //             payload: { ...event, quantity: parseInt(e.target.value) },
+    //           })
+    //         }
+    //       />
+    //     </div>
+    //   </td>
+    //   <td>${event.quantity * event.price}</td>
+    //   <td>
+    //     <button
+    //       className="btn btn-link"
+    //       onClick={() =>
+    //         CartStore.dispatch({ type: "delete", payload: event })
+    //       }>
+    //       <span className="bi bi-trash-fill font-large text-dark">
+    //         <CloseOutlinedIcon />
+    //       </span>
+    //     </button>
+    //   </td>
+    // </tr>
   );
 };
 export default Shoppingcartitem;
